c2ccd2daa9756b6d8c7975f53b771992cb648927
sudev <sudev1997@gmail.com>
2025-12-30 16:30:20 +0530

    java

---
+++ Java/basics/addTwo.java
@@ -0,0 +1,10 @@
+
+class AddTwoNumber {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("sum is " + sum);
+    }
+}
---
+++ Java/basics/checkEven.java
@@ -0,0 +1,16 @@
+
+// Check Even or Odd
+
+
+public class checkEven {
+    public static void main(String[] args){
+        int num = 44;
+
+        if(num %2 == 0){
+            System.out.print(" Number is:");
+            System.out.println("\n Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
